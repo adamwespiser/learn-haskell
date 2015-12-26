e04d4bda569ddb7e4c5b000006c045a79cad534e
Adam Wespiser <adamwespiser@gmail.com>
2015-12-26 01:02:19 -0500

    add in lambda calc, church boolean encoding as anonymous haskell functions

---
+++ types-prog-lang/lambda-calc.hs
@@ -0,0 +1,38 @@
+-- Types and Programming Languages
+-- Chapter 5: Untyped Lambda Calculus
+--
+tru = \t -> \f -> t
+fls = \t -> \f -> f
+test = \l -> \m -> \n -> l m n
+and' = \b -> \c -> b c fls
+not' = \b -> \t -> \f -> b f t
+
+
+-- test tru v w
+-- (\l -> \m -> \n -> l m n) tru v w
+-- (\m -> \n -> tru m n) v w
+-- (\n -> tru v n) w
+-- (tru v w)
+-- (\t -> \f -> t) v w
+-- (\f -> v) w 
+-- v
+--
+-- and tru tru
+-- (\b -> \c -> b c fls) tru tru
+-- (\c -> tru c fls) tru
+-- (tru tru fls)
+-- (\t -> \f -> t) tru fls
+-- (\f -> tru) fls
+-- tru
+--
+-- not tru
+-- eq. to (\t -> \f -> f)
+--
+-- not = (\b -> \t -> \f -> b f t)
+-- not tru
+-- (\b -> \t -> \f -> b f t) tru
+-- (\t -> \f -> tru f t)
+-- or == (\p -> \q -> p p q)
+--
+--
+--
